bd8b5db6fc69c5e0b4d7be2b40ee035dfc88bb43
XA54889 <jhonnatanjesus.salvatierra.contractor@gdevtools.com>
2021-10-12 15:47:58 -0500

    React hook app

---
+++ src/hooks/useForm.js
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+export const useForm = (initialState = {}) => {
+
+    const [values, setValues] = useState(initialState);
+
+    const reset = (state) => {
+        setValues( initialState );
+    }
+
+    const handleInputChange = ({ target }) => {
+        setValues({
+            ...values,
+            [ target.name ]: target.value
+        })
+    }
+
+    return [values, handleInputChange, reset];
+}
